c244c5967ba85f697f62fc9a90896638857d9281
ZhouLiqian <96431886+ZhouLiqian@users.noreply.github.com>
2024-03-28 20:40:55 +0800

    Create multiprocess.py

---
+++ multiprocess.py
@@ -0,0 +1,22 @@
+"""基于进程的并行"""
+
+# Process类
+from multiprocessing import Process
+import os
+
+def info(title):
+    print(title)
+    print('module name:', __name__)
+    print('parent process:', os.getppid())
+    print('process id:', os.getpid())
+
+def f(name):
+    info('function f')
+    print('hello', name)
+    
+if __name__ == "__main__":
+    info('main line')
+    p = Process(target = f, args = ('bob',))
+    # 生成进程
+    p.start()
+    p.join()
